0b9d8ef49d2cc30f711b3786095f1de90e314307
Silevilence <silevilence@outlook.com>
2018-03-17 13:05:28 +0800

    fix some bugs after test

--- volumes.user.js
+++ volumes.user.js
@@ -2,7 +2,7 @@
 // @name         g-cores-volumes
 // @name:zh-cn   机核电台页面增强
 // @namespace    https://github.com/silevilence/enhances-g-cores
-// @version      1.0
+// @version      1.1
 // @description  add functions to volumes page of g-cores
 // @description:zh-cn  在机核的电台页面上增加（大概）有用的功能
 // @author       Silevilence
@@ -13,50 +13,60 @@
 (function () {
     'use strict';
 
-    // 链接调用的总方法，点击后获取所有时间轴图片地址并在新页面展示
-    // noinspection ES6ConvertVarToLetConst, JSUnusedLocalSymbols
-    var show_img_urls = function () {
-        // 首先获取时间轴的图片（在class为audio_dot_img的div下）
-        // 根据播放进度有textarea和img的区别，需要全部获取并整合
-        // 最后需要加上题图（img.img-responsive）
-        let divs = $('div.audio_dot_img');
-
-        // img in textarea
-        // noinspection JSValidateTypes
-        let textareas = divs.children('textarea');
-        // get img urls
-        // img标签以文本的方式放在textarea中，取出文本后用引号分割来获取url
-        let urls = textareas.map(function () {
-            return $(this).text().split(/['"]+/)[1];
-        });
-
-        // img in img
-        // 已播放前后的图片被解析为img标签，直接获取即可
-        // noinspection JSValidateTypes
-        let imgs = divs.children('img');
-        for (let i of imgs) {
-            urls.push(i.attr('src'));
-        }
-
-        // 题图
-        urls.push($('img.img-responsive').attr('src'));
-
-        // remove _limit in url
-        let urls_modified = $.map(urls, function (value) {
-            return value.split('_limit').join('');
-        });
-
-        // open new tab and show urls
-        let str = ['<pre>', urls_modified.join('\n'), '</pre>'];
-        let win = window.open('', '_blank');
-        win.document.write(str.join('\n'));
-    };
-
     // generate element a and set it to show urls in new tab
     // element a
     let download_link = document.createElement('a');
     // set href to show img urls
-    $(download_link).attr('href', 'javascript:show_img_urls();');
+    // $(download_link).attr('href', 'javascript:show_img_urls();');
+    // $(download_link).click(show_img_urls());
+    $(download_link).attr('href', '#');
+    $(function () {
+        $(download_link).click(
+            // 链接调用的总方法，点击后获取所有时间轴图片地址并在新页面展示
+            // 由于写在外面会由于不明原因被调用一次，故作为匿名函数放入
+            // noinspection ES6ConvertVarToLetConst, JSUnusedLocalSymbols
+            function () {
+                // 首先获取时间轴的图片（在class为audio_dot_img的div下）
+                // 根据播放进度有textarea和img的区别，需要全部获取并整合
+                // 最后需要加上题图（img.img-responsive）
+                let divs = $('div.audio_dot_img');
+
+                // img in textarea
+                // noinspection JSValidateTypes
+                let textareas = divs.children('textarea');
+                // get img urls
+                // img标签以文本的方式放在textarea中，取出文本后用引号分割来获取url
+                let urls = textareas.map(function () {
+                    return $(this).text().split(/['"]+/)[1];
+                });
+
+                // img in img
+                // 已播放前后的图片被解析为img标签，直接获取即可
+                // noinspection JSValidateTypes
+                let imgs = divs.children('img');
+                imgs.each(function () {
+                    urls.push($(this).attr('src'));
+                });
+
+                // 题图
+                urls.push($('img.img-responsive').attr('src'));
+
+                // remove _limit in url
+                let urls_modified = $.map(urls, function (value) {
+                    return value.split('_limit').join('');
+                });
+
+                // open new tab and show urls
+                let str = ['<pre>', urls_modified.join('\n'), '</pre>'];
+                let win = window.open('', '_blank');
+                if (!win) {
+                    alert('弹出窗口被拦截，请允许该网站弹出窗口。');
+                    return;
+                }
+                win.document.write(str.join('\n'));
+            }
+        );
+    });
     // set contents of element
     $(download_link).html(`<span class="fa fa-download"></span>
     时间轴图片下载`);

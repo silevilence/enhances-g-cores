4da53fbbf98a510f76c5b0ad463d827f0e78ffdd
Silevilence <silevilence@outlook.com>
2018-03-17 10:35:04 +0800

    initial js commit

---
+++ volumes.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         g-cores-volumes
+// @name:zh-cn   机核电台页面增强
+// @namespace    https://github.com/silevilence/enhances-g-cores
+// @version      0.1
+// @description  add functions to volumes page
+// @description:zh-cn  在机核的电台页面上增加（大概）有用的功能
+// @author       Silevilence
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
+// @match        http*://www.g-cores.com/volumes/*
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    alert('function on');
+})();

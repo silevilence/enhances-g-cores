6943f629db68743bb91b8ddf11fd84439f559061
Silevilence <silevilence@outlook.com>
2018-03-21 10:53:12 +0800

    去掉了造成麻烦的英文描述

--- volumes.user.js
+++ volumes.user.js
@@ -1,10 +1,8 @@
 // ==UserScript==
-// @name         g-cores-volumes
-// @name:zh-cn   机核电台页面增强
+// @name   机核电台页面增强
 // @namespace    https://github.com/silevilence/enhances-g-cores
-// @version      1.2.1
-// @description  add functions to volumes page of g-cores
-// @description:zh-cn  在机核的电台页面上增加（大概）有用的功能
+// @version      1.2.2
+// @description  在机核的电台页面上增加（大概）有用的功能
 // @author       Silevilence
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
 // @match        http*://www.g-cores.com/volumes/*

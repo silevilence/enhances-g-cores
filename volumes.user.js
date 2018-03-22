6114af79ce3c45782bb2905560b7ae0ede12c77a
Silevilence <silevilence@outlook.com>
2018-03-22 08:40:52 +0800

    机核网页已经引用了jquery，不需要另外引用

--- volumes.user.js
+++ volumes.user.js
@@ -4,7 +4,6 @@
 // @version      1.2.2
 // @description  在机核的电台页面上增加（大概）有用的功能
 // @author       Silevilence
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
 // @match        http*://www.g-cores.com/volumes/*
 // @supportURL   https://github.com/silevilence/enhances-g-cores/issues
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=silevilence@outlook.com&item_name=Greasy+Fork+donation

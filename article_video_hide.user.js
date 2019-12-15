c3f1bcce64e77a7f420603894b4054425b65cd07
Silevilence <silevilence@outlook.com>
2019-12-15 20:57:38 +0800

    文章页视频隐藏更新，使用了新的url和新的隐藏方式 有bug，可能会失败，以后再解决

--- article_video_hide.user.js
+++ article_video_hide.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         机核文章页视频自动隐藏
 // @namespace    https://github.com/silevilence/enhances-g-cores
-// @version      1.0
+// @version      1.1
 // @description  自动隐藏文章页面上的视频（被自动播放烦得不要不要的）
 // @author       Silevilence
-// @match        http*://www.g-cores.com/articles/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
+// @match        http*://www.gcores.com/articles/*
 // @supportURL   https://github.com/silevilence/enhances-g-cores/issues
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=silevilence@outlook.com&item_name=Greasy+Fork+donation
 // ==/UserScript==
@@ -12,12 +13,67 @@
 (function () {
     'use strict';
 
-    // 获取所有iframe，每个iframe包含一个视频
-    let videos = $('iframe');
+    // 机核现在不用jQuery了，需要自己引入
+    /**
+     * 隐藏视频用的函数
+     * @param video 带视频的外层div
+     * @param timeout 延时
+     */
+    function hideVideo(video, timeout) {
+        // 现在外层是figure
+        let figure = $(video).parent();
+
+        window.setTimeout(function () {
+
+            figure.addClass('story_block-spoiler');
+
+            if ($(video).find('iframe').length === 0) {
+                hideVideo(video, timeout * 2);
+                return;
+            }
+
+            // 提示按钮
+            let div = document.createElement('div');
+            $(div).addClass('story_hidden');
+            $(div).attr('role', 'button');
+            div.tabIndex = -1;
+
+            // 提示文字
+            let p = document.createElement('p');
+            $(p).text('这里有一个被自动隐藏的视频');
+
+            let strong = document.createElement('strong');
+            $(strong).text('点击查看');
+
+            div.append(p);
+            div.append(strong);
+
+            $(function () {
+                $(div).click(function () {
+                    figure.removeClass('story_block-spoiler');
+                });
+            });
+
+            figure.prepend(div);
+        }, timeout);
+    }
+
+    // 每个视频放在一个div中，class带有story_embed-video
+    let videos = $('div.story_embed-video');
     // 用each针对每个视频进行单独的隐藏处理，处理方式模仿机核的剧透隐藏功能
     videos.each(function () {
-        // 获取父级div一边进行后续操作
-        let div = $(this).parent();
+
+        // 已经被隐藏了
+        if ($(this).parent().hasClass('story_block-spoiler')) {
+            console.log('A video is already hidden.');
+            return;
+        }
+
+        hideVideo(this, 2000);
+
+        // 新版换了个处理方式，下面的代码全部木大
+        // 其实能用，只是会很难看而已
+        /*
         // 获取外层html并作为字符串保存在一个隐藏的textarea中
         let str = this.outerHTML;
         let textarea = document.createElement('textarea');
@@ -43,7 +99,8 @@
             });
         });
         // 对父级div通过前端插入的方式依次放入a和textarea
-        div.prepend($(a));
-        div.prepend($(textarea));
+        figure.prepend($(a));
+        figure.prepend($(textarea));
+        */
     });
 })();
